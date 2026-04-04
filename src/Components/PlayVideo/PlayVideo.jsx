04c1f00590cd8ea636eb9f8a917c6f1b51faaa19
Ahmad Bajwa <ahmedbajwa871@gmail.com>
2026-04-04 16:15:30 +0500

    latest updation

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,9 +1,142 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './PlayVideo.css'
+import video1 from '../../assets/video.mp4'
+import like from '../../assets/like.png'
+import dislike from '../../assets/dislike.png'
+import share from '../../assets/share.png'
+import save from '../../assets/save.png'
+import jack from '../../assets/jack.png'
+import user_profile from '../../assets/user_profile.jpg'
+import { API_KEY, value_converter } from '../../data'
+import moment from 'moment'
 
-const PlayVideo = () => {
+const PlayVideo = ({ videoId }) => {
+
+  const [apiData, setApiData] = useState(null);
+  const [channelData, setChannelData] = useState(null);
+  const [commentData,setCommentData] = useState([]);
+
+    const fetchVideoData = async () => {
+      //Fetching Videos Data
+      const videoDetails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`
+      await fetch(videoDetails_url).then(res => res.json()).then(data => setApiData(data.items[0]))
+    }
+
+    const fetchOtherData = async () =>{
+       //Fetching Channel Data
+      const channlDetails_url = ` https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${apiData.snippet.channelId}&key=${API_KEY}`
+      await fetch(channlDetails_url).then(res => res.json()).then(data => setChannelData(data.items[0]))
+    
+       //Fetching Comments Data
+      const comments_url = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&${videoId}&key=${API_KEY} ` 
+      await fetch(comments_url).then(res => res.json()).then(data =>setCommentData(data.items[0]))
+    }
+
+  useEffect(() => {
+    fetchVideoData();
+  }, [])
+
+  useEffect(() =>{
+    fetchOtherData();
+  })
+
   return (
-    <div>
-        
+    <div className='play-video'>
+      {/* <video src={video1} controls autoPlay muted></video> */}
+      <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <h3>{apiData ? apiData.snippet.title : "Title Here"}</h3>
+      <div className='play-video-info'>
+        <p>{apiData ? value_converter(apiData.statistics.viewCount) : "16k"} Views &bull; {apiData ? moment(apiData.snippet.publishedAt).fromNow() : ""}</p>
+        <div>
+          <span><img src={like} alt="" /> {apiData ? value_converter(apiData.statistics.likeCount) : 155}</span>
+          <span><img src={dislike} alt="" /> </span>
+          <span><img src={share} alt="" /> Share</span>
+          <span><img src={save} alt="" /> Save</span>
+        </div>
+      </div>
+      <hr />
+      <div className='publisher'>
+        <img src={channelData?channelData.snippet.thumbnails.default.url:""} alt="" />
+        <div>
+          <p>{apiData?apiData.snippet.channelTitle:""}</p>
+          <span>{channelData?value_converter(channelData.statistics.subscriberCount):155} Subscribers</span>
+        </div>
+        <button>Subscribe</button>
+      </div>
+      <div className="vid-description">
+        <p>
+          {apiData?.snippet?.description
+            ? apiData.snippet.description.slice(0, 250) + "..."
+            : "Description not available"}
+        </p>
+        <hr />
+        <h4>{apiData?value_converter(apiData.statistics.commentCount):120} Comments</h4>
+        <div className='comment'>
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>Jack Nicholson <span>1 day ago</span></h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat sint aliquam consequuntur? Sint odit molestiae quo reiciendis. Totam porro iste delectus nulla non ea doloremque atque, vel animi error.</p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>244</span>
+              <img src={dislike} alt="" />
+
+            </div>
+          </div>
+        </div>
+        <div className='comment'>
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>Jack Nicholson <span>1 day ago</span></h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat sint aliquam consequuntur? Sint odit molestiae quo reiciendis. Totam porro iste delectus nulla non ea doloremque atque, vel animi error.</p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>244</span>
+              <img src={dislike} alt="" />
+
+            </div>
+          </div>
+        </div>
+        <div className='comment'>
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>Jack Nicholson <span>1 day ago</span></h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat sint aliquam consequuntur? Sint odit molestiae quo reiciendis. Totam porro iste delectus nulla non ea doloremque atque, vel animi error.</p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>244</span>
+              <img src={dislike} alt="" />
+
+            </div>
+          </div>
+        </div>
+        <div className='comment'>
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>Jack Nicholson <span>1 day ago</span></h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat sint aliquam consequuntur? Sint odit molestiae quo reiciendis. Totam porro iste delectus nulla non ea doloremque atque, vel animi error.</p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>244</span>
+              <img src={dislike} alt="" />
+
+            </div>
+          </div>
+        </div>
+        <div className='comment'>
+          <img src={user_profile} alt="" />
+          <div>
+            <h3>Jack Nicholson <span>1 day ago</span></h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat sint aliquam consequuntur? Sint odit molestiae quo reiciendis. Totam porro iste delectus nulla non ea doloremque atque, vel animi error.</p>
+            <div className="comment-action">
+              <img src={like} alt="" />
+              <span>244</span>
+              <img src={dislike} alt="" />
+
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
